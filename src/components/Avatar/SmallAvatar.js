be954a60eb36b2ccc6187eadeb95b61b7ed2d29b
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-03-22 02:37:26 +0900

    Update SmallAvatar.js

--- src/components/Avatar/SmallAvatar.js
+++ src/components/Avatar/SmallAvatar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import siteConfig from "../../../data/SiteConfig";
 import AvatarLinks from "./AvatarLinks";
+import logo from "../../../data/logo.png";
 
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui";
@@ -25,10 +26,7 @@ const SmallAvatar = props => {
       </Styled.h2>
 
       <div sx={{ display: `flex`, alignItems: `center`, mb: 2, mt: 2 }}>
-        <img
-          src={avatar.photo}
-          sx={{ width: 70, height: 70, borderRadius: 999, mr: `15px` }}
-        />
+      <img src= { logo }/>
         <Styled.p>{avatar.description}</Styled.p>
       </div>
       <div sx={{ textAlign: `left` }}>
